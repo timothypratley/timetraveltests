e5861f8b73426099d69aa5d7cba3b8f9bb287e01
Timothy Pratley <timothypratley@gmail.com>
2011-03-16 18:51:07 -0700

    Initial creation

---
+++ PerformanceTests/PerformanceTests/CompareMaps.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.FSharp.Collections;
+using System.Diagnostics;
+
+namespace PerformanceTests
+{
+    [TestClass]
+    public class CompareMaps
+    {
+        public class Item
+        {
+            public int A { get; set; }
+            public string B { get; set; }
+            public Item(int a, string b)
+            {
+                A = a;
+                B = b;
+            }
+        }
+
+        [TestMethod]
+        public void Performance()
+        {
+            //Var.pushThreadBindings(RT.map(RT.CURRENT_NS, RT.CURRENT_NS.deref()));
+            var m1 = new RecordedMapModel<string, Item>();
+            //var m2 = new ClojureRecordedMapModel<string, Item>();
+            var m3 = new Dictionary<string, Item>();
+            var m4 = new FSharpMap<string, Item>(Enumerable.Empty<Tuple<string, Item>>());
+            //var m5 = new PersistentTreeMap();
+
+            const int size = 10000;
+            var s = new Stopwatch();
+            s.Start();
+            for (var i = 0; i < size; i++)
+                m1.Set(i.ToString(), new Item(i, i.ToString()));
+            for (var i = 0; i < size * 100; i++)
+                m1.Get((i % size).ToString());
+            for (var i = 0; i < size; i++)
+                m1.Remove(i.ToString());
+            s.Stop();
+            Console.WriteLine("RecordedMapModel took " + s.Elapsed);
+
+            /*
+            s.Restart();
+            for (var i = 0; i < size; i++)
+                m2.Set(i.ToString(), new Item(i, i.ToString()));
+            for (var i = 0; i < size * 100; i++)
+                m2.Get((i % size).ToString());
+            for (var i = 0; i < size; i++)
+                m2.Remove(i.ToString());
+            s.Stop();
+            Console.WriteLine("ClojureRecordedMapModel took " + s.Elapsed);
+             */
+
+            s.Restart();
+            for (var i = 0; i < size; i++)
+                m3[i.ToString()] = new Item(i, i.ToString());
+            Item o;
+            for (var i = 0; i < size * 100; i++)
+                m3.TryGetValue((i % size).ToString(), out o);
+            for (var i = 0; i < size; i++)
+                m3.Remove(i.ToString());
+            s.Stop();
+            Console.WriteLine("Dictionary took " + s.Elapsed);
+
+            m3.Clear();
+            s.Restart();
+            for (var i = 0; i < size; i++)
+            {
+                var tmp = new Dictionary<string, Item>(m3);
+                tmp[i.ToString()] = new Item(i, i.ToString());
+                m3 = tmp;
+            }
+            for (var i = 0; i < size * 100; i++)
+                m3.TryGetValue((i % size).ToString(), out o);
+            for (var i = 0; i < size; i++)
+            {
+                var tmp = new Dictionary<string, Item>(m3);
+                tmp.Remove(i.ToString());
+                m3 = tmp;
+            }
+            s.Stop();
+            Console.WriteLine("Copy Dictionary took " + s.Elapsed);
+
+            s.Restart();
+            for (var i = 0; i < size; i++)
+                m4 = m4.Add(i.ToString(), new Item(i, i.ToString()));
+            for (var i = 0; i < size * 100; i++)
+                m4.TryFind((i % size).ToString());
+            for (var i = 0; i < size; i++)
+                m4 = m4.Remove(i.ToString());
+            s.Stop();
+            Console.WriteLine("FSharpMap took " + s.Elapsed);
+
+            /*
+            s.Restart();
+            for (var i = 0; i < size; i++)
+                m5 = (PersistentTreeMap)m5.assoc(i.ToString(), new Item(i, i.ToString()));
+            object obj;
+            for (var i = 0; i < size * 100; i++)
+                m5.TryGetValue((i % size).ToString(), out obj);
+            for (var i = 0; i < size; i++)
+                m5 = (PersistentTreeMap)RT.dissoc(m5, i.ToString());
+            s.Stop();
+            Console.WriteLine("PersistentTreeMap took " + s.Elapsed);
+             */
+        }
+    }
+}
---
+++ PerformanceTests/PerformanceTests/RecordedMapModel.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using Microsoft.FSharp.Collections;
+using Microsoft.FSharp.Core;
+using System.Linq;
+
+namespace PerformanceTests
+{
+    /// <summary>
+    /// Objects stored in this Map need to be immutable to ensure they are not modified externally,
+    /// so that all changes are recorded in the history.
+    /// History is stored in an ordered map of version to state,
+    /// where the state is a map of keys to values.
+    /// </summary>
+    public class RecordedMapModel<TKeyType, TValueType> : IEnumerable<TValueType>
+    {
+        private FSharpMap<int, FSharpMap<TKeyType, TValueType>> _history
+            = new FSharpMap<int, FSharpMap<TKeyType, TValueType>>(
+                Enumerable.Empty<Tuple<int,FSharpMap<TKeyType,TValueType>>>());
+        private FSharpMap<TKeyType, TValueType> _currentMap
+            = new FSharpMap<TKeyType,TValueType>(Enumerable.Empty<Tuple<TKeyType,TValueType>>());
+
+        public int CurrentVersion { get; private set; }
+        public RecordedMapModel()
+        {
+            CurrentVersion = -1;
+            Clear();
+        }
+
+        public FSharpMap<TKeyType, TValueType> Map
+        {
+            get { return _currentMap; }
+            set
+            {
+                _currentMap = value;
+                _history = _history.Add(++CurrentVersion, value);
+            }
+        }
+
+        public RecordedMapModel(IEnumerable<KeyValuePair<TKeyType, TValueType>> keyValuePairs) : this()
+        {
+            Contract.Requires<ArgumentNullException>(keyValuePairs != null);
+
+            foreach (var e in keyValuePairs)
+                Map = Map.Add(e.Key, e.Value);
+        }
+
+        public void Set(TKeyType key, TValueType value)
+        {
+            Map = Map.Add(key, value);
+        }
+
+        public void Remove(TKeyType key)
+        {
+            Map = Map.Remove(key);
+        }
+
+        public TValueType Get(TKeyType id)
+        {
+            var option = Map.TryFind(id);
+            return option == FSharpOption<TValueType>.None ? default(TValueType) : option.Value;
+        }
+
+        public IEnumerable<TValueType> Values { get { return Map.Select(x=>x.Value); } }
+
+        public int Count { get { return Map.Count; } }
+
+        public void Clear()
+        {
+            Map = new FSharpMap<TKeyType, TValueType>(Enumerable.Empty<Tuple<TKeyType, TValueType>>());
+        }
+
+        public bool ContainsKey(TKeyType key)
+        {
+            return Map.ContainsKey(key);
+        }
+
+        #region IEnumerable<TValueType> Members
+
+        public IEnumerator<TValueType> GetEnumerator()
+        {
+            return Map.Select(x=>x.Value).GetEnumerator();
+        }
+
+        #endregion
+
+        #region IEnumerable Members
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return Map.Select(x=>x.Value).GetEnumerator();
+        }
+
+        #endregion
+    }
+}
